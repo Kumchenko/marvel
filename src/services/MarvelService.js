75388a626d7d421b7d2fc350526d8d7e95992039
Kirill Kumchenko <kirillkumchenko@gmail.com>
2023-02-17 16:06:18 +0200

    randomChar

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,8 @@
 
 
 class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=617e2d38a316c4ce64d877fad420f901';
     getResource = async (url) => {
         const res = await fetch(url);
     
@@ -22,8 +24,31 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResource('https://gateway.marvel.com:443/v1/public/characters?apikey=617e2d38a316c4ce64d877fad420f901')
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
+    }
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        let { name, description, thumbnail, urls } = char;
+        if (description.length > 231) {
+            description = description.slice(0, 228) + '...';
+        }
+        if (!description) {
+            description = "This character is so confidential, that's why we can't find information about him.";
+        }
+        return {
+            name: name,
+            description: description,
+            thumbnail: `${thumbnail.path}.${thumbnail.extension}`,
+            homepage: urls[0].url,
+            wiki: urls[1].url
+        }
     }
 }
 
