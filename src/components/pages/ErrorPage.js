96f961f1091b0eb770372587e92c2aa2c29b21ad
Kirill Kumchenko <kirillkumchenko@gmail.com>
2023-03-04 01:19:41 +0200

    Tabulation

--- src/components/pages/ErrorPage.js
+++ src/components/pages/ErrorPage.js
@@ -4,18 +4,18 @@ import ErrorMessage from "../errorMessage/ErrorMessage";
 const ErrorPage = () => {
     const navigate = useNavigate();
     return (
-        <div style={{display: 'flex', alignItems: 'center'}}>
-            <div style={{textAlign: 'right', margin: '0 auto'}}>
-            <p style={{fontSize: 25}}>You entered the page that doesn't exist</p>
-            <button
-                className="button button__main button__long"
-                onClick={() => navigate("/")}
-            >
-                <div className="inner">Homepage</div>
-            </button>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+            <div style={{ textAlign: 'right', margin: '0 auto' }}>
+                <p style={{ fontSize: 25 }}>You entered the page that doesn't exist</p>
+                <button
+                    className="button button__main button__long"
+                    onClick={() => navigate("/")}
+                >
+                    <div className="inner">Homepage</div>
+                </button>
             </div>
-            <ErrorMessage/>
-            
+            <ErrorMessage />
+
         </div>
     )
 }
