d14acab35926d99fb9c6ec88c55c8d9922567c65
Kirill Kumchenko <kirillkumchenko@gmail.com>
2023-03-04 01:19:19 +0200

    Fixing styles

--- src/components/pages/ErrorPage.js
+++ src/components/pages/ErrorPage.js
@@ -4,9 +4,9 @@ import ErrorMessage from "../errorMessage/ErrorMessage";
 const ErrorPage = () => {
     const navigate = useNavigate();
     return (
-        <div style={{display: 'flex', 'align-items': 'center', 'justify-content': 'center'}}>
-            <div style={{'text-align': 'right', margin: '0 auto'}}>
-            <p style={{'font-size': '25px'}}>You entered the page that doesn't exist</p>
+        <div style={{display: 'flex', alignItems: 'center'}}>
+            <div style={{textAlign: 'right', margin: '0 auto'}}>
+            <p style={{fontSize: 25}}>You entered the page that doesn't exist</p>
             <button
                 className="button button__main button__long"
                 onClick={() => navigate("/")}
